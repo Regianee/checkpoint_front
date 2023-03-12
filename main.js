172a106ebfbaafee927ee19da79e26667adc15e9
Regiane <regiane.leitte@outlook.com>
2023-03-11 21:26:46 -0300

    responsividade

--- main.js
+++ main.js
@@ -12,35 +12,32 @@ function alteraTema() {
     // Verifica qual o valor que é retornado pelo Switch
     //console.log(switchValue.checked);
 
-    if (toggleswitch.checked) { //Caso seja verdadeiro, ou seja, o botão foi clicado
 
+
+    //------------if else para quando o botão for clicado------------------\\
+
+
+    if (toggleswitch.checked) { // caso seja verdadeiro, ou seja, o botão foi clicado\\
         /* Adicionando as classes (dark-mode) css nos elementos capturados */
+        /*dúvida ----> pq o h2 e o p só são atingidos na segunda forEach? verificar se é pq as outras estão fora do card*/
         cardsList.forEach(card => {
             card.classList.add('cards-dark');
             body.classList.add('body-dark');
             h1.classList.add('h1-dark');
-            h2.classList.add('h2-dark');
-            p.classList.add('p-dark');
-        });
-
-        cardsList.forEach(card => {
             card.querySelector('h2').classList.add('h2-dark')
             card.querySelector('p').classList.add('p-dark')
         });
 
         /* Remove as classes adicionadas anteiormente, assim, a página volta para o "modo-claro" */
+        //Percorre a lista de elementos, para fazer a remoção em cada um dos itens
     } else {
 
-        //body.classList.remove('body-dark');//funcionando
-        // h1.classList.remove('h1-dark');//só o primeiro card
-        // h2.classList.remove('h2-dark');//só o primeiro card
-        // p.classList.remove('p-dark');//só o primeiro card
-
-        //Percorre a lista de elementos, para fazer a remoção em cada um dos itens
         cardsList.forEach(card => {
+            body.classList.remove('body-dark');
+            h1.classList.remove('h1-dark');
+            card.querySelector('h2').classList.remove('h2-dark')
+            card.querySelector('p').classList.remove('p-dark')
             card.classList.remove('cards-dark');
-            p.classList.remove('p-dark');
-            h2.classList.remove('h2-dark');
         });
     }
 }
